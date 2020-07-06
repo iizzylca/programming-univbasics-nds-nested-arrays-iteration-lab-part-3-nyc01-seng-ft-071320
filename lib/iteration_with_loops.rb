04d6cc867f7aae00ccfbd6e4d60c53db1ee185cf
Israel Canessa <israellcanessa@gmail.com>
2020-07-06 21:18:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,8 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src.count do
-      binding.pry
-      if src[row_index][element_index] == " "
+      #binding.pry
+      if src[row_index]== " "
         string_array << src[row_index][element_index]
       end
       element_index +=1
